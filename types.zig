0bf9965b06da9e579a4878e58a07c4c20d9a857a
Samadi van Koten <samadi@vktec.org.uk>
2021-08-28 17:49:45 +0100

    Add programUniform2i

--- types.zig
+++ types.zig
@@ -92,6 +92,7 @@ pub const Program = enum(UInt) {
 
     pub const uniform1ui = gl.programUniform1ui;
     pub const uniform1i = gl.programUniform1i;
+    pub const uniform2i = gl.programUniform2i;
     pub const uniform1f = gl.programUniform1f;
     pub const uniform2f = gl.programUniform2f;
     pub const uniform3f = gl.programUniform3f;
