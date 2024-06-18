4c1a75dbf45a568781917f967c881bb6a08abfe9
Fernando <fraguilar@pm.me>
2024-06-18 03:14:41 +0200

    update to Zig 2024.05

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zgl", .{
-        .root_source_file = .{ .path = "src/zgl.zig" },
+        .root_source_file = b.path("src/zgl.zig"),
         .target = target,
         .optimize = optimize,
     });
